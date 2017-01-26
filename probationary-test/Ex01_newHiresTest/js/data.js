a19f0a4b63f17d60e5f790b2ca0a0adaf234f274
shawn.thecool <shawn.thecool@kakaocorp.com>
2017-01-26 10:13:13 +0900

    lots update

---
+++ probationary-test/Ex01_newHiresTest/js/data.js
@@ -0,0 +1,64 @@
+/**
+ * Created by zach on 16. 11. 7..
+ */
+var STORE = {
+	"라이언": [
+		{
+			"name": "Kakao Friends Milk Chocolate Set",
+			"price": 12000,
+			"stock": 12,
+			"description": "앞뒷면이 다른 패키지가 매력적인 카카오프렌즈 밀크 초콜렛\n이중 포장되어 있어 안심하고 먹을 수 있습니다.\n달콤함으로 무장한 카카오프렌즈를 사랑하는 사람에게 선물해보세요",
+			"category": "홈/스낵",
+			"images": ["store_ryan_0.jpg"],
+			"ageRestriction": 0,
+			"specification":{
+				"volume": "90g"
+			},
+			"linkUrl": "https://store.kakaofriends.com/kr/products/FRPBFRMCS0001"
+		},
+		{
+			"name": "뉴플란넬담요_라이언",
+			"price": 18000,
+			"stock": 0,
+			"description": "플란넬 담요가 라이언과 함께 돌아왔어요~\n파스텔 톤 컬러의 촉감이 보드라운 담요입니다.",
+			"category": "홈/패브릭",
+			"images": ["store_ryan_1.jpg"],
+			"ageRestriction": 0,
+			"specification":{
+				"size": "105x75cm(-3cm)"
+			},
+			"linkUrl": "https://store.kakaofriends.com/kr/products/FRPBRYFBL0418"
+		},
+		{
+			"name": "논슬립데스크매트_라이언",
+			"price": 9900,
+			"stock": 20,
+			"description": "논슬립매트로 책상 위에서 밀리지 않으며 마우스패드 겸용으로 사용할 수 있습니다.\n귀여운 라이언 데스크매트를 책상 위에 올려보세요~\n주의: 본 상품은 커팅매트가 아닙니다.",
+			"category": "홈/스낵",
+			"images": ["store_ryan_2.jpg"],
+			"ageRestriction": 14,
+			"specification":{
+				"size": "55*32.5cm"
+			},
+			"linkUrl": "https://store.kakaoㄴfriends.com/kr/products/FRPBRNNDM0001"
+		}
+	],
+	"무지":[
+
+	],
+	"어피치": [
+		{
+			"name": "뉴플란넬담요_어피치",
+			"price": 18000,
+			"stock": 0,
+			"description": "플란넬 담요가 더욱 사랑스러워진 어피차와 함께 돌아왔어요~\n 파스텔 톤 컬러의 촉감이 보드라운 담요입니다.",
+			"category": "홈/패브릭",
+			"images": ["store_apeach_0.jpg"],
+			"ageRestriction": 0,
+			"specification":{
+				"size": "100x75cm"
+			},
+			"linkUrl": "https://store.kakaofriends.com/kr/products/FRPBAPFBL0419"
+		}
+	]
+};
