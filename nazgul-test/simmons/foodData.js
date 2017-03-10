9f609bd6e892173477363c9ccab173f96cc5dd89
shawn.thecool <shawn.thecool@kakaocorp.com>
2017-03-10 10:04:31 +0900

    lots update

---
+++ nazgul-test/simmons/foodData.js
@@ -0,0 +1,25 @@
+/**
+ * Created by shawn on 2017. 2. 26..
+ */
+var DATA = [
+    {
+        "foodname": "김치찌개",
+        "foodImage": "www.kimchi"
+    },
+    {
+        "foodname": "된장찌개",
+        "foodImage": "www.denjang"
+    },
+    {
+        "foodname": "미역국",
+        "foodImage": "www.miyork"
+    },
+    {
+        "foodname": "북어국",
+        "foodImage": "www.bukukook"
+    },
+    {
+        "foodname": "순두부찌개",
+        "foodImage": "www.soontuboo"
+    }
+];
